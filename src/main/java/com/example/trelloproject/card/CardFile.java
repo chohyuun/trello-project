a80d1f11b173fdf71591e4bbbc46d4b3206e933f
cooleHoonst <88084780+89JHoon@users.noreply.github.com>
2024-12-24 16:30:20 +0900

    :sparkles: Update : CardFile 생성

---
+++ src/main/java/com/example/trelloproject/card/CardFile.java
@@ -0,0 +1,26 @@
+package com.example.trelloproject.card;
+
+import com.example.trelloproject.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "cardfile")
+@NoArgsConstructor
+public class CardFile extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "card_id")
+    private Card card;
+
+    @Column(name = "file")
+    private String file;
+
+
+
+}
