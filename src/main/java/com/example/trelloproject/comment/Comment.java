2cb6c4ac712ac8aa3ddccffce41d5796d8a8de24
조현지 <qndjddl001@gmail.com>
2024-12-24 11:02:32 +0900

    ✨ Update: 댓글 엔티티 생성

--- src/main/java/com/example/trelloproject/comment/Comment.java
+++ src/main/java/com/example/trelloproject/comment/Comment.java
@@ -1,4 +1,42 @@
 package com.example.trelloproject.comment;
 
-public class Comment {
+import com.example.trelloproject.card.Card;
+import com.example.trelloproject.global.entity.BaseEntity;
+import com.example.trelloproject.member.Member;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name="comment")
+@Getter
+@Entity
+@NoArgsConstructor
+public class Comment extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String contents;
+
+    @ManyToOne
+    @JoinColumn(name="member_id", nullable = false)
+    private Member member;
+
+    @ManyToOne
+    @JoinColumn(name="card_id", nullable = false)
+    private Card card;
+
+    public Comment(String contents, Member member, Card card) {
+        this.contents = contents;
+        this.member = member;
+        this.card = card;
+    }
 }
