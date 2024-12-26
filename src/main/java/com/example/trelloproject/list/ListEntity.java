9fb74c6cce8720321d633a889400666d68474b18
Dawnfeeling <swhan98@naver.com>
2024-12-26 17:32:21 +0900

    ✨ Update : List CRUD

--- src/main/java/com/example/trelloproject/list/ListEntity.java
+++ src/main/java/com/example/trelloproject/list/ListEntity.java
@@ -1,22 +1,19 @@
 package com.example.trelloproject.list;
 
 import com.example.trelloproject.board.Board;
+import com.example.trelloproject.card.Card;
 import com.example.trelloproject.global.entity.BaseEntity;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
 @Table(name = "list")
-public class List extends BaseEntity {
+public class ListEntity extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -25,18 +22,25 @@ public class List extends BaseEntity {
     @Column(nullable = false)
     private String title;
 
+    @Setter
     @Column(name = "list_index")
     private Integer index;
 
-    @Setter
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "board_id", nullable = false)
     private Board board;
 
-    public List() {}
+    @OneToMany(mappedBy = "list", cascade = CascadeType.REMOVE, orphanRemoval = true)
+    private final List<Card> cards = new ArrayList<>();
 
-    public List(String title, int index) {
+    public ListEntity() {}
+
+    public ListEntity(Board board, String title) {
+        this.board = board;
         this.title = title;
-        this.index = index;
+    }
+
+    public void updateTitle(String title) {
+        this.title = title;
     }
 }
---
+++ src/main/java/com/example/trelloproject/list/ListService.java
@@ -0,0 +1,76 @@
+package com.example.trelloproject.list;
+
+import com.example.trelloproject.board.Board;
+import com.example.trelloproject.board.BoardRepository;
+import com.example.trelloproject.global.dto.MessageDto;
+import com.example.trelloproject.list.dto.ListResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class ListService {
+
+    private final ListRepository listRepository;
+    private final BoardRepository boardRepository;
+
+    //TODO : 권한 및 예외처리
+
+    //리스트 생성
+    public ListResponseDto createList(Long boardId, String title) {
+        //권한체킹 필요
+
+        Board board = boardRepository.findByIdOrElseThrow(boardId);
+
+        ListEntity list = new ListEntity(board, title);
+        int maxIndex = listRepository.findMaxIndex();
+        list.setIndex(maxIndex + 1);
+
+        ListEntity savedList = listRepository.save(list);
+
+        return new ListResponseDto(savedList.getId(), savedList.getBoard().getId(), savedList.getTitle(), savedList.getIndex());
+    }
+
+    //리스트 제목 변경
+    @Transactional
+    public ListResponseDto changeTitle(Long listId, String title) {
+        ListEntity list = listRepository.findByIdOrElseThrow(listId);
+
+        list.updateTitle(title);
+
+        return new ListResponseDto(list.getId(), list.getBoard().getId(), list.getTitle(), list.getIndex());
+    }
+
+    //리스트 순서 변경
+    @Transactional
+    public ListResponseDto changeIndex(Long listId, Integer index) {
+        ListEntity list = listRepository.findByIdOrElseThrow(listId);
+
+        int oldIndex = list.getIndex();
+
+        if(oldIndex == index){
+            return new ListResponseDto(list.getId(), list.getBoard().getId(), list.getTitle(), list.getIndex());
+        }
+
+        if(index > oldIndex){
+            listRepository.decrementIndexRange(oldIndex + 1, index);
+        }else{
+            listRepository.incrementIndexRange(index, oldIndex - 1);
+        }
+        list.setIndex(index);
+
+        return new ListResponseDto(list.getId(), list.getBoard().getId(), list.getTitle(), list.getIndex());
+    }
+
+    //리스트 삭제
+    public MessageDto deleteList(Long listId) {
+        ListEntity list = listRepository.findByIdOrElseThrow(listId);
+
+        listRepository.delete(list);
+
+        String message = "삭제 완료되었습니다.";
+
+        return new MessageDto(message);
+    }
+}
