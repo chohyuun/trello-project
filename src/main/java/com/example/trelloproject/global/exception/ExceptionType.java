3f2d752923b96ab2e4f25da16b198a0304653a72
cooleHoonst <88084780+89JHoon@users.noreply.github.com>
2024-12-27 20:19:59 +0900

    :recycle: Refactor : ExceptionType.java  불필요한 부분 삭제

--- src/main/java/com/example/trelloproject/global/exception/ExceptionType.java
+++ src/main/java/com/example/trelloproject/global/exception/ExceptionType.java
@@ -6,7 +6,6 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum ExceptionType {
 
-	EXIST_USER(HttpStatus.BAD_REQUEST,"이 email을 사용할 수 없습니다."),
 	//cardException 관련
 	INVALID_FILE_FORMAT(HttpStatus.BAD_REQUEST, "지원되지 않는 파일 형식입니다. (지원 형식: jpg, png, pdf, csv)"),
 	FILE_SIZE_EXCEEDED(HttpStatus.BAD_REQUEST, "파일 크기는 5MB를 초과할 수 없습니다."),
@@ -14,16 +13,6 @@ public enum ExceptionType {
 	FILE_NOT_FOUND(HttpStatus.NOT_FOUND, "첨부파일을 찾을 수 없습니다."),
 	FILE_DELETE_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "파일 삭제 중 오류가 발생했습니다."),
 	CARD_NOT_FOUND(HttpStatus.NOT_FOUND, "카드를 찾을 수 없습니다."),
-	//fake
-	NOT_FIND_BOARD(HttpStatus.BAD_REQUEST,"보드를 찾을수 없습니다."),
-	NOT_FIND_WORKSPACE(HttpStatus.BAD_REQUEST, "워크스페이스를 찾을수 없습니다."),
-	ExceptionType(HttpStatus.BAD_REQUEST,"권한없음"),
-	NOT_ADMIN(HttpStatus.BAD_REQUEST,"권한업슴"),
-	UNAUTHORIZED(HttpStatus.UNAUTHORIZED,"권한?"),
-	NOT_FIND_MEMBER(HttpStatus.BAD_REQUEST,"맴버없음"),
-	NOT_MEMBER(HttpStatus.BAD_REQUEST,"맴벙벗음"),
-	NOT_FIND_LIST(HttpStatus.BAD_REQUEST,"리스트업승"),
-
 
 	UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "접근 권한이 없습니다."),
 	NOT_ADMIN(HttpStatus.UNAUTHORIZED, "관리자만 이용할 수 있습니다."),
