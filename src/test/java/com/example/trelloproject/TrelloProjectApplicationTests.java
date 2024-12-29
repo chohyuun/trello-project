9e0723e5ba4e3313237a7efc254d1ab7e3f3dbb2
조현지 <qndjddl001@gmail.com>
2024-12-30 02:55:47 +0900

    🎉 Feat : 배포를 위한 파일 추가

--- src/test/java/com/example/trelloproject/TrelloProjectApplicationTests.java
+++ src/test/java/com/example/trelloproject/TrelloProjectApplicationTests.java
@@ -1,13 +1,8 @@
 package com.example.trelloproject;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class TrelloProjectApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
