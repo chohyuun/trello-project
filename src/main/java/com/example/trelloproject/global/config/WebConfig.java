5e77f0c69ef06d21ebe89726fe1d9df05867870e
박예진 <qkrdpwls2002>
2024-12-30 14:58:05 +0900

    🩹 fix : 코드 주석 추가, 사용하지 않는 코드 정리

--- src/main/java/com/example/trelloproject/global/config/WebConfig.java
+++ src/main/java/com/example/trelloproject/global/config/WebConfig.java
@@ -1,37 +1,3 @@
-
-//package com.example.trelloproject.global.config;
-//
-//import com.example.trelloproject.global.filter.LoginFilter;
-//import jakarta.servlet.Filter;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.boot.web.servlet.FilterRegistrationBean;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//
-//@Configuration
-//@RequiredArgsConstructor
-//public class WebConfig {
-//
-//	@Bean
-//	public FilterRegistrationBean loginFilter(){
-//
-//		// 필터를 등록하고 설정
-//		FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
-//
-//		// HTTP 요청에 대해 로그인 작업 처리, LoginFilter 인스턴스를 필터로 설정
-//		filterRegistrationBean.setFilter(new LoginFilter());
-//
-//		// 모든 URL 패턴에 대해 필터가 동작하도록 설정
-//		filterRegistrationBean.addUrlPatterns("/*");
-//
-//		filterRegistrationBean.setOrder(1);
-//
-//		// 필터 설정이 완료된 FilterRegistrationBean 객체 반환
-//		return filterRegistrationBean;
-//	}
-//
-//}
 package com.example.trelloproject.global.config;
 
 import com.example.trelloproject.card.MultipartJackson2HttpMessageConverter;
@@ -49,15 +15,7 @@ import java.util.List;
 
 @RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
-	private  LoginInterceptor loginInterceptor;
 	private  MultipartJackson2HttpMessageConverter multipartJackson2HttpMessageConverter;
-
-	@Override
-	public void addInterceptors(InterceptorRegistry registry) {
-		registry.addInterceptor(loginInterceptor);
-	}
-
-
 
 	//카드 파일 관련
 	@Bean
